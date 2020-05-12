0e0713208785296edde8980c86f45c4544c48b3f
David Yeshi <davidyeshi0@gmail.com>
2020-05-12 16:26:34 +1000

    Tweak the postMapping to return a proper rest response

--- demoRest/src/main/java/com/example/demoRest/controller/EmployeeController.java
+++ demoRest/src/main/java/com/example/demoRest/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.example.demoRest.payroll.EmployeeModelAssembler;
 import com.example.demoRest.payroll.EmployeeRepository;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,8 +47,15 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) throws URISyntaxException {
+        EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+
+        // ResponseEntity is used to create an HTTP 201 Created status message
+        // this type of response typically includes a Location response header, and we use the URI derived
+        // from the model's self related link
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     // Single item
