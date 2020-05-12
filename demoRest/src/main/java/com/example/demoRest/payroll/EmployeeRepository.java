cb3258f2a18adc4776ed70a6095405e31e073189
David Yeshi <davidyeshi0@gmail.com>
2020-05-12 11:30:57 +1000

    Creating rest controller and setting up the GET,POST,PUT,DELETE Mappings

--- demoRest/src/main/java/com/example/demoRest/payroll/EmployeeRepository.java
+++ demoRest/src/main/java/com/example/demoRest/payroll/EmployeeRepository.java
@@ -2,6 +2,6 @@ package com.example.demoRest.payroll;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
