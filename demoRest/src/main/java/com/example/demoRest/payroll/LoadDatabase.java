3276662411eb9761dd91f4528bd720d18ea12e43
David Yeshi <davidyeshi0@gmail.com>
2020-05-12 11:15:41 +1000

    Creating two entities in jpa repos on load

---
+++ demoRest/src/main/java/com/example/demoRest/payroll/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.example.demoRest.payroll;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class LoadDatabase {
+
+    // Spring boot runs all CommandLineRunner beans once the application context is loaded
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
