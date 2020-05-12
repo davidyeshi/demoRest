7521cab27823b61102f600ef8df14db85b049e60
David Yeshi <davidyeshi0@gmail.com>
2020-05-12 16:21:23 +1000

    Changing Employee class

--- demoRest/src/main/java/com/example/demoRest/payroll/Employee.java
+++ demoRest/src/main/java/com/example/demoRest/payroll/Employee.java
@@ -12,14 +12,28 @@ public class Employee {
 
     // == fields ==
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     // == constructors ==
     Employee() {}
 
-    Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
+    }
+
+    // == public methods ==
+
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public void setName(String name) {
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 }
