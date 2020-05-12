9a150e2901b8d9b88f12e1732db51556e51ac149
David Yeshi <davidyeshi0@gmail.com>
2020-05-12 17:35:06 +1000

    Adding Orders endpoints and building restApi links

--- demoRest/src/main/java/com/example/demoRest/payroll/LoadDatabase.java
+++ demoRest/src/main/java/com/example/demoRest/payroll/LoadDatabase.java
@@ -13,10 +13,17 @@ public class LoadDatabase {
 
     // Spring boot runs all CommandLineRunner beans once the application context is loaded
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepository) {
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Bilbo","Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo","Baggins", "thief")));
+
+            orderRepository.save(new Order("Macbook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
         };
     }
 }
